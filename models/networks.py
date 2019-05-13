e48f9b73aecfb8952fd687b3ace333edb2cd277d
Xu Zhang <xu.zhang@columbia.edu>
2019-05-13 14:42:33 +0000

    add original pggan discriminator

--- models/networks.py
+++ models/networks.py
@@ -4,6 +4,7 @@ from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
 from torch.nn.init import kaiming_normal_, calculate_gain
+import numpy as np
 
 ###############################################################################
 # Helper Functions
@@ -190,6 +191,8 @@ def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal'
         net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer)
     elif netD == 'pixel':     # classify if each pixel is real or fake
         net = PixelDiscriminator(input_nc, ndf, norm_layer=norm_layer)
+    elif netD == 'pggan':     # classify if each pixel is real or fake
+        net = Discriminator(input_nc, mbstat_avg='all', resolution=256, fmap_max=512, fmap_base=8192, sigmoid_at_end=True)
     else:
         raise NotImplementedError('Discriminator model name [%s] is not recognized' % net)
     return init_net(net, init_type, init_gain, gpu_ids)
@@ -693,3 +696,259 @@ class PixelDiscriminator(nn.Module):
     def forward(self, input):
         """Standard forward."""
         return self.net(input)
+
+def D_conv(incoming, in_channels, out_channels, kernel_size, padding, nonlinearity, init, param=None, 
+        to_sequential=True, use_wscale=True, use_gdrop=True, use_layernorm=False, gdrop_param=dict()):
+    layers = incoming
+    if use_gdrop:
+        layers += [GDropLayer(**gdrop_param)]
+    layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)]
+    he_init(layers[-1], init, param)  # init layers
+    if use_wscale:
+        layers += [WScaleLayer(layers[-1])]
+    layers += [nonlinearity]
+    if use_layernorm:
+        layers += [LayerNormLayer()]  # TODO: requires incoming layer
+    if to_sequential:
+        return nn.Sequential(*layers)
+    else:
+        return layers
+
+def NINLayer(incoming, in_channels, out_channels, nonlinearity, init, param=None, 
+            to_sequential=True, use_wscale=True):
+    layers = incoming
+    layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)]  # NINLayer in lasagne
+    he_init(layers[-1], init, param)  # init layers
+    if use_wscale:
+        layers += [WScaleLayer(layers[-1])]
+    if not (nonlinearity == 'linear'):
+        layers += [nonlinearity]
+    if to_sequential:
+        return nn.Sequential(*layers)
+    else:
+        return layers
+
+class DSelectLayer(nn.Module):
+    def __init__(self, pre, chain, inputs):
+        super(DSelectLayer, self).__init__()
+        assert len(chain) == len(inputs)
+        self.pre = pre
+        self.chain = chain
+        self.inputs = inputs
+        self.N = len(self.chain)
+
+    def forward(self, x, y=None, cur_level=None, insert_y_at=None):
+        if cur_level is None:
+            cur_level = self.N  # cur_level: physical index
+        if y is not None:
+            assert insert_y_at is not None
+
+        max_level, min_level = int(np.floor(self.N-cur_level)), int(np.ceil(self.N-cur_level))
+        min_level_weight, max_level_weight = int(cur_level+1)-cur_level, cur_level-int(cur_level)
+        
+        _from, _to, _step = min_level+1, self.N, 1
+
+        if self.pre is not None:
+            x = self.pre(x)
+
+        if max_level == min_level:
+            x = self.inputs[max_level](x)
+            if max_level == insert_y_at:
+                x = self.chain[max_level](x, y)
+            else:
+                x = self.chain[max_level](x)
+        else:
+            out = {}
+            tmp = self.inputs[max_level](x)
+            if max_level == insert_y_at:
+                tmp = self.chain[max_level](tmp, y)
+            else:
+                tmp = self.chain[max_level](tmp)
+            out['max_level'] = tmp
+            out['min_level'] = self.inputs[min_level](x)
+            x = resize_activations(out['min_level'], out['max_level'].size()) * min_level_weight + \
+                                out['max_level'] * max_level_weight
+            if min_level == insert_y_at:
+                x = self.chain[min_level](x, y)
+            else:
+                x = self.chain[min_level](x)
+
+        for level in range(_from, _to, _step):
+            if level == insert_y_at:
+                x = self.chain[level](x, y)
+            else:
+                x = self.chain[level](x)
+
+        return x
+
+class GDropLayer(nn.Module):
+    """
+    # Generalized dropout layer. Supports arbitrary subsets of axes and different
+    # modes. Mainly used to inject multiplicative Gaussian noise in the network.
+    """
+    def __init__(self, mode='mul', strength=0.2, axes=(0,1), normalize=False):
+        super(GDropLayer, self).__init__()
+        self.mode = mode.lower()
+        assert self.mode in ['mul', 'drop', 'prop'], 'Invalid GDropLayer mode'%mode
+        self.strength = strength
+        self.axes = [axes] if isinstance(axes, int) else list(axes)
+        self.normalize = normalize
+        self.gain = None
+
+    def forward(self, x, deterministic=False):
+        if deterministic or not self.strength:
+            return x
+
+        rnd_shape = [s if axis in self.axes else 1 for axis, s in enumerate(x.size())]  # [x.size(axis) for axis in self.axes]
+        if self.mode == 'drop':
+            p = 1 - self.strength
+            rnd = np.random.binomial(1, p=p, size=rnd_shape) / p
+        elif self.mode == 'mul':
+            rnd = (1 + self.strength) ** np.random.normal(size=rnd_shape)
+        else:
+            coef = self.strength * x.size(1) ** 0.5
+            rnd = np.random.normal(size=rnd_shape) * coef + 1
+
+        if self.normalize:
+            rnd = rnd / np.linalg.norm(rnd, keepdims=True)
+        rnd = Variable(torch.from_numpy(rnd).type(x.data.type()))
+        if x.is_cuda:
+            rnd = rnd.cuda()
+        return x * rnd
+
+    def __repr__(self):
+        param_str = '(mode = %s, strength = %s, axes = %s, normalize = %s)' % (self.mode, self.strength, self.axes, self.normalize)
+        return self.__class__.__name__ + param_str
+
+class MinibatchStatConcatLayer(nn.Module):
+    """Minibatch stat concatenation layer.
+    - averaging tells how much averaging to use ('all', 'spatial', 'none')
+    """
+    def __init__(self, averaging='all'):
+        super(MinibatchStatConcatLayer, self).__init__()
+        self.averaging = averaging.lower()
+        if 'group' in self.averaging:
+            self.n = int(self.averaging[5:])
+        else:
+            assert self.averaging in ['all', 'flat', 'spatial', 'none', 'gpool'], 'Invalid averaging mode'%self.averaging
+        self.adjusted_std = lambda x, **kwargs: torch.sqrt(torch.mean((x - torch.mean(x, **kwargs)) ** 2, **kwargs) + 1e-8) #Tstdeps in the original implementation
+
+    def forward(self, x):
+        shape = list(x.size())
+        target_shape = shape.copy()
+        vals = self.adjusted_std(x, dim=0, keepdim=True)# per activation, over minibatch dim
+        if self.averaging == 'all':  # average everything --> 1 value per minibatch
+            target_shape[1] = 1
+            vals = torch.mean(vals, dim=1, keepdim=True)#vals = torch.mean(vals, keepdim=True)
+
+        elif self.averaging == 'spatial':  # average spatial locations
+            if len(shape) == 4:
+                vals = mean(vals, axis=[2,3], keepdim=True)  # torch.mean(torch.mean(vals, 2, keepdim=True), 3, keepdim=True)
+        elif self.averaging == 'none':  # no averaging, pass on all information
+            target_shape = [target_shape[0]] + [s for s in target_shape[1:]]
+        elif self.averaging == 'gpool':  # EXPERIMENTAL: compute variance (func) over minibatch AND spatial locations.
+            if len(shape) == 4:
+                vals = mean(x, [0,2,3], keepdim=True)  # torch.mean(torch.mean(torch.mean(x, 2, keepdim=True), 3, keepdim=True), 0, keepdim=True)
+        elif self.averaging == 'flat':  # variance of ALL activations --> 1 value per minibatch
+            target_shape[1] = 1
+            vals = torch.FloatTensor([self.adjusted_std(x)])
+        else:  # self.averaging == 'group'  # average everything over n groups of feature maps --> n values per minibatch
+            target_shape[1] = self.n
+            vals = vals.view(self.n, self.shape[1]/self.n, self.shape[2], self.shape[3])
+            vals = mean(vals, axis=0, keepdim=True).view(1, self.n, 1, 1)
+        vals = vals.expand(*target_shape)
+        return torch.cat([x, vals], 1) # feature-map concatanation
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(averaging = %s)' % (self.averaging)
+
+class Discriminator(nn.Module):
+    def __init__(self, 
+                num_channels    = 1,        # Overridden based on dataset.
+                resolution      = 32,       # Overridden based on dataset.
+                label_size      = 0,        # Overridden based on dataset.
+                fmap_base       = 4096,
+                fmap_decay      = 1.0,
+                fmap_max        = 256,
+                mbstat_avg      = 'all',
+                mbdisc_kernels  = None,
+                use_wscale      = True,
+                use_gdrop       = True,
+                use_layernorm   = False,
+                sigmoid_at_end  = False):
+        super(Discriminator, self).__init__()
+        self.num_channels = num_channels
+        self.resolution = resolution
+        self.label_size = label_size
+        self.fmap_base = fmap_base
+        self.fmap_decay = fmap_decay
+        self.fmap_max = fmap_max
+        self.mbstat_avg = mbstat_avg
+        self.mbdisc_kernels = mbdisc_kernels
+        self.use_wscale = use_wscale
+        self.use_gdrop = use_gdrop
+        self.use_layernorm = use_layernorm
+        self.sigmoid_at_end = sigmoid_at_end
+
+        R = int(np.log2(resolution))
+        assert resolution == 2**R and resolution >= 4
+        gdrop_strength = 0.0
+
+        negative_slope = 0.2
+        act = nn.LeakyReLU(negative_slope=negative_slope)
+        # input activation
+        iact = 'leaky_relu'
+        # output activation
+        output_act = nn.Sigmoid() if self.sigmoid_at_end else 'linear'
+        output_iact = 'sigmoid' if self.sigmoid_at_end else 'linear'
+        gdrop_param = {'mode': 'prop', 'strength': gdrop_strength}
+
+        nins = nn.ModuleList()
+        lods = nn.ModuleList()
+        pre = None
+
+        nins.append(NINLayer([], self.num_channels, self.get_nf(R-1), act, iact, negative_slope, True, self.use_wscale))
+
+        for I in range(R-1, 1, -1):
+            ic, oc = self.get_nf(I), self.get_nf(I-1)
+            net = D_conv([], ic, ic, 3, 1, act, iact, negative_slope, False, 
+                        self.use_wscale, self.use_gdrop, self.use_layernorm, gdrop_param)
+            net = D_conv(net, ic, oc, 3, 1, act, iact, negative_slope, False, 
+                        self.use_wscale, self.use_gdrop, self.use_layernorm, gdrop_param)
+            net += [nn.AvgPool2d(kernel_size=2, stride=2, ceil_mode=False, count_include_pad=False)]
+            lods.append(nn.Sequential(*net))
+            # nin = [nn.AvgPool2d(kernel_size=2, stride=2, ceil_mode=False, count_include_pad=False)]
+            nin = []
+            nin = NINLayer(nin, self.num_channels, oc, act, iact, negative_slope, True, self.use_wscale)
+            nins.append(nin)
+
+        net = []
+        ic = oc = self.get_nf(1)
+        if self.mbstat_avg is not None:
+            net += [MinibatchStatConcatLayer(averaging=self.mbstat_avg)]
+            ic += 1
+        net = D_conv(net, ic, oc, 3, 1, act, iact, negative_slope, False, 
+                    self.use_wscale, self.use_gdrop, self.use_layernorm, gdrop_param)
+        net = D_conv(net, oc, self.get_nf(0), 4, 0, act, iact, negative_slope, False,
+                    self.use_wscale, self.use_gdrop, self.use_layernorm, gdrop_param)
+
+        # Increasing Variation Using MINIBATCH Standard Deviation
+        if self.mbdisc_kernels:
+            net += [MinibatchDiscriminationLayer(num_kernels=self.mbdisc_kernels)]
+
+        oc = 1 + self.label_size
+        # lods.append(NINLayer(net, self.get_nf(0), oc, 'linear', 'linear', None, True, self.use_wscale))
+        lods.append(NINLayer(net, self.get_nf(0), oc, output_act, output_iact, None, True, self.use_wscale))
+
+        self.output_layer = DSelectLayer(pre, lods, nins)
+
+    def get_nf(self, stage):
+        return min(int(self.fmap_base / (2.0 ** (stage * self.fmap_decay))), self.fmap_max)
+
+    def forward(self, x, y=None, cur_level=None, insert_y_at=None, gdrop_strength=0.0):
+        for module in self.modules():
+            if hasattr(module, 'strength'):
+                module.strength = gdrop_strength
+        return self.output_layer(x, y, cur_level, insert_y_at)
+
+
